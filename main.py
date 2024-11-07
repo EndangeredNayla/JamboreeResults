0511d228ce87d0bae41694337205ec1a81257550
Nayla Hanegan <naylahanegan@gmail.com>
2024-11-06 22:46:36 -0500

    test

--- main.py
+++ main.py
@@ -45,103 +45,103 @@ def generate_data():
     board_image = Image.open(board_image_path)
     
     try:
-        p1_star_2 = Image.open(f'number/{int(star_amounts[0][1])}.png')
+        p1_star_2 = Image.open(f'number/{int(star_amounts[0][0])}.png')
         board_image.paste(p1_star_2, (460, 120), p1_star_2)
-        p1_star_1 = Image.open(f'number/{int(star_amounts[0][0])}.png')
+        p1_star_1 = Image.open(f'number/{int(star_amounts[0][1])}.png')
         board_image.paste(p1_star_1, (500, 120), p1_star_1)
     except:
         p1_star_1 = Image.open(f'number/{int(star_amounts[0][0])}.png')
         board_image.paste(p1_star_1, (460, 120), p1_star_1)
 
     try:
-        p2_star_2 = Image.open(f'number/{int(star_amounts[1][1])}.png')
+        p2_star_2 = Image.open(f'number/{int(star_amounts[1][0])}.png')
         board_image.paste(p2_star_2, (460, 260), p2_star_2) 
-        p2_star_1 = Image.open(f'number/{int(star_amounts[1][0])}.png')
+        p2_star_1 = Image.open(f'number/{int(star_amounts[1][1])}.png')
         board_image.paste(p2_star_1, (500, 260), p2_star_1)
     except:
-        p2_star_1 = Image.open(f'number/{int(star_amounts[1][0])}.png')
+        p2_star_1 = Image.open(f'number/{int(star_amounts[1][10])}.png')
         board_image.paste(p2_star_1, (460, 260), p2_star_1)
 
     try:
-        p3_star_2 = Image.open(f'number/{int(star_amounts[2][1])}.png')
+        p3_star_2 = Image.open(f'number/{int(star_amounts[2][0])}.png')
         board_image.paste(p3_star_2, (460, 400), p3_star_2) 
-        p3_star_1 = Image.open(f'number/{int(star_amounts[2][0])}.png')
+        p3_star_1 = Image.open(f'number/{int(star_amounts[2][1])}.png')
         board_image.paste(p3_star_1, (500, 400), p3_star_1)
     except:
         p3_star_1 = Image.open(f'number/{int(star_amounts[2][0])}.png')
         board_image.paste(p3_star_1, (460, 400), p3_star_1)
 
     try:
-        p4_star_2 = Image.open(f'number/{int(star_amounts[3][1])}.png')
+        p4_star_2 = Image.open(f'number/{int(star_amounts[3][0])}.png')
         board_image.paste(p4_star_2, (460, 540), p4_star_2)
-        p4_star_1 = Image.open(f'number/{int(star_amounts[3][0])}.png')
+        p4_star_1 = Image.open(f'number/{int(star_amounts[3][1])}.png')
         board_image.paste(p4_star_1, (500, 540), p4_star_1) 
     except:
         p4_star_1 = Image.open(f'number/{int(star_amounts[3][0])}.png')
         board_image.paste(p4_star_1, (460, 540), p4_star_1)
 
     try:
-        p1_coin_3 = Image.open(f'number/{int(coin_amounts[0][2])}.png')
+        p1_coin_3 = Image.open(f'number/{int(coin_amounts[0][0])}.png')
         board_image.paste(p1_coin_3, (895, 120), p1_coin_3)
         p1_coin_2 = Image.open(f'number/{int(coin_amounts[0][1])}.png')
         board_image.paste(p1_coin_2, (935, 120), p1_coin_2)
-        p1_coin_1 = Image.open(f'number/{int(coin_amounts[0][0])}.png')
+        p1_coin_1 = Image.open(f'number/{int(coin_amounts[0][2])}.png')
         board_image.paste(p1_coin_1, (975, 120), p1_coin_1)
     except:
         try:
-            p1_coin_2 = Image.open(f'number/{int(coin_amounts[0][1])}.png')
+            p1_coin_2 = Image.open(f'number/{int(coin_amounts[0][0])}.png')
             board_image.paste(p1_coin_2, (895, 120), p1_coin_2)
-            p1_coin_1 = Image.open(f'number/{int(coin_amounts[0][0])}.png')
+            p1_coin_1 = Image.open(f'number/{int(coin_amounts[0][1])}.png')
             board_image.paste(p1_coin_1, (935, 120), p1_coin_1)
         except:
             p1_coin_1 = Image.open(f'number/{int(coin_amounts[0][0])}.png')
             board_image.paste(p1_coin_1, (895, 120), p1_coin_1)
 
     try:
-        p2_coin_3 = Image.open(f'number/{int(coin_amounts[1][2])}.png')
+        p2_coin_3 = Image.open(f'number/{int(coin_amounts[1][0])}.png')
         board_image.paste(p2_coin_3, (895, 260), p2_coin_3) 
         p2_coin_2 = Image.open(f'number/{int(coin_amounts[1][1])}.png')
         board_image.paste(p2_coin_2, (935, 260), p2_coin_2)
-        p2_coin_1 = Image.open(f'number/{int(coin_amounts[1][0])}.png')
+        p2_coin_1 = Image.open(f'number/{int(coin_amounts[1][2])}.png')
         board_image.paste(p2_coin_1, (975, 260), p2_coin_1)
     except:
         try:
-            p2_coin_2 = Image.open(f'number/{int(coin_amounts[1][1])}.png')
+            p2_coin_2 = Image.open(f'number/{int(coin_amounts[1][0])}.png')
             board_image.paste(p2_coin_2, (895, 260), p2_coin_2)
-            p2_coin_1 = Image.open(f'number/{int(coin_amounts[1][0])}.png')
+            p2_coin_1 = Image.open(f'number/{int(coin_amounts[1][1])}.png')
             board_image.paste(p2_coin_1, (935, 260), p2_coin_1)
         except:
             p2_coin_1 = Image.open(f'number/{int(coin_amounts[1][0])}.png')
             board_image.paste(p2_coin_1, (895, 260), p2_coin_1)
     try:
-        p3_coin_3 = Image.open(f'number/{int(coin_amounts[2][2])}.png')
+        p3_coin_3 = Image.open(f'number/{int(coin_amounts[2][0])}.png')
         board_image.paste(p3_coin_3, (895, 400), p3_coin_3) 
         p3_coin_2 = Image.open(f'number/{int(coin_amounts[2][1])}.png')
         board_image.paste(p3_coin_2, (935, 400), p3_coin_2)
-        p3_coin_1 = Image.open(f'number/{int(coin_amounts[2][0])}.png')
+        p3_coin_1 = Image.open(f'number/{int(coin_amounts[2][2])}.png')
         board_image.paste(p3_coin_1, (975, 400), p3_coin_1)
     except:
         try:
-            p3_coin_2 = Image.open(f'number/{int(coin_amounts[2][1])}.png')
+            p3_coin_2 = Image.open(f'number/{int(coin_amounts[2][0])}.png')
             board_image.paste(p3_coin_2, (895, 400), p3_coin_2)
-            p3_coin_1 = Image.open(f'number/{int(coin_amounts[2][0])}.png')
+            p3_coin_1 = Image.open(f'number/{int(coin_amounts[2][1])}.png')
             board_image.paste(p3_coin_1, (935, 400), p3_coin_1)
         except:
             p3_coin_1 = Image.open(f'number/{int(coin_amounts[2][0])}.png')
             board_image.paste(p3_coin_1, (895, 400), p3_coin_1)
 
     try:
-        p4_coin_3 = Image.open(f'number/{int(coin_amounts[3][2])}.png')
+        p4_coin_3 = Image.open(f'number/{int(coin_amounts[3][0])}.png')
         board_image.paste(p4_coin_3, (895, 540), p4_coin_3)
         p4_coin_2 = Image.open(f'number/{int(coin_amounts[3][1])}.png')
         board_image.paste(p4_coin_2, (935, 540), p4_coin_2)
-        p4_coin_1 = Image.open(f'number/{int(coin_amounts[3][0])}.png')
+        p4_coin_1 = Image.open(f'number/{int(coin_amounts[3][2])}.png')
         board_image.paste(p4_coin_1, (975, 540), p4_coin_1)
     except:
         try:
-            p4_coin_2 = Image.open(f'number/{int(coin_amounts[3][1])}.png')
+            p4_coin_2 = Image.open(f'number/{int(coin_amounts[3][0])}.png')
             board_image.paste(p4_coin_2, (895, 540), p4_coin_2)
-            p4_coin_1 = Image.open(f'number/{int(coin_amounts[3][0])}.png')
+            p4_coin_1 = Image.open(f'number/{int(coin_amounts[3][1])}.png')
             board_image.paste(p4_coin_1, (935, 540), p4_coin_1)
         except:
             p4_coin_1 = Image.open(f'number/{int(coin_amounts[3][0])}.png')
